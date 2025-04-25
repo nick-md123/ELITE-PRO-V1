00ba81381cfd0a79b29a15ad7371b927b36ff037
nick-md123 <monjannick6@gmail.com>
2025-04-26 00:41:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,14 +5,14 @@ const chalk = require('chalk')
 //contact details
 global.ownernomer = "917907997537"
 global.ownername = "Krishna xer"
-global.ytname = "YT: @ElitePro-Empire"
-global.socialm = "GitHub: Elite-MD"
+global.ytname = "YT: @krishnaa-xd"
+global.socialm = "GitHub: nick-md123"
 global.location = "Nigeria, Port Harcourt"
 
 
 global.ownernumber = '917907997537'  //creator number
 global.ownername = 'Krishna xer' //owner name
-global.botname = 'ELITE-PRO-V1' //name of the bot
+global.botname = 'KRISHNA-MD' //name of the bot
 
 
 //custom prefix
@@ -30,14 +30,14 @@ global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
-global.welcome = true // or false to disable
+global.welcome = false // or false to disable
 
 //sticker details
 global.packname = 'Sticker By'
 global.author = 'Krishna-xer\n\nContact: +917907997537'
 
 //set your menu picture
-global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' //menu and thumb picture
+global.elitepropp = 'https://files.catbox.moe/ytylzw.jpeg' //menu and thumb picture
 
 
 
